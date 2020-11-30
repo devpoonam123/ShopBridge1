eeba87e23a38faaa95f61dffc01d5fd832222083
@devPoonam123 <Poonamdwi0109@gmail.com>
2020-11-30 17:44:38 +0530

    changes

--- ShopBridge/Controllers/InventoryController.cs
+++ ShopBridge/Controllers/InventoryController.cs
@@ -1,6 +1,7 @@
 ﻿using ShopBridge.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -16,43 +17,19 @@ namespace ShopBridge.Controllers
         public ActionResult Index()
         {
             IEnumerable<Inventory> inventories = null;
-            //try
-            //{
-            //    using (var client = new HttpClient())
-            //    {
-            //        client.BaseAddress = new Uri("http://localhost:6068/");
-            //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //        var result = client.GetAsync("api/Inventory/GetAll").Result;
-            //        if (result.IsSuccessStatusCode)
-            //        {
-            //            var readTask = result.Content.ReadAsAsync<IList<Inventory>>();
-            //            readTask.Wait();
+            try
+            {
 
-            //            inventories = readTask.Result;
-            //        }
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:6068/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("api/Inventory/GetAll").Result;
+                inventories = response.Content.ReadAsAsync<IList<Inventory>>().Result;
+            }
+            catch(Exception ex)
+            {
 
-            //        else
-            //        {
-            //            inventories = Enumerable.Empty<Inventory>();
-            //            ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-
-            //}
-
-            //  return View(inventories);
-
-
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:6068/");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = client.GetAsync("api/Inventory/GetAll").Result;
-            inventories = response.Content.ReadAsAsync<IList<Inventory>>().Result;
-            // return View("~/Views/GetEmploye.cshtml", cd);
+            }
             return View(inventories);
 
         }
@@ -62,57 +39,69 @@ namespace ShopBridge.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SaveImageInFolder()
+        {
+            string filename = string.Empty;
+
+            try
+            {
+                if (Request.Files.Count != 0)
+                {
+
+                    for (int i = 0; i < Request.Files.Count; i++)
+                    {
+                        var file = Request.Files[i];
+
+                        filename = Path.GetFileName(file.FileName);
+
+                        var path = Path.Combine(Server.MapPath("~/Images/"), filename);
+                        file.SaveAs(path);
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(filename);
+        }
+
+
+
         public ActionResult Details(int id)
         {
             Inventory inventory = null;
-            //using (var client = new HttpClient())
-            //{
-
-            //    client.BaseAddress = new Uri("http://localhost:6068/");
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //    //var response = await client.GetAsync(string.format("api/Inventory/GetInventoryById/id={0}&type={1}", param.Id.Value, param.Id.Type));
-            //    //var result = client.GetAsync("api/Inventory/GetInventoryById/"+"1").Result;
-            //    var result = client.GetAsync(string.Format("api/Inventory/GetInventoryById?id={0}", id)).Result;
-                
-            //    if (result.IsSuccessStatusCode)
-            //    {
-            //        var readTask = result.Content.ReadAsAsync<Inventory>();
-            //        readTask.Wait();
-
-            //        inventory = readTask.Result;
-            //    }
-
-            //    else
-            //    {
-            //        inventory = new Inventory();
-            //        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-
-            //    }
-            //}
-           
-
-
-            using (var client = new HttpClient { BaseAddress = new Uri("http://localhost:6068/") })
+            try
             {
-                //string serailizeddto = JsonConvert.SerializeObject(dto);
-
-                var inputMessage = new HttpRequestMessage
+                using (var client = new HttpClient { BaseAddress = new Uri("http://localhost:6068/") })
                 {
-                    Content = new StringContent(Convert.ToString(id), Encoding.UTF8, "application/json")
-                };
 
-                inputMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var inputMessage = new HttpRequestMessage
+                    {
+                        Content = new StringContent(Convert.ToString(id), Encoding.UTF8, "application/json")
+                    };
 
-                HttpResponseMessage message = client.PostAsync("api/Inventory/GetInventoryById", inputMessage.Content).Result;
+                    inputMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                if (!message.IsSuccessStatusCode)
-                    throw new ArgumentException(message.ToString());
-                if (message.IsSuccessStatusCode)
-                {
-                    var readTask = message.Content.ReadAsAsync<Inventory>();
-                    readTask.Wait();
-                    inventory = readTask.Result;
+                    HttpResponseMessage message = client.PostAsync("api/Inventory/GetInventoryById", inputMessage.Content).Result;
+
+                    if (!message.IsSuccessStatusCode)
+                        throw new ArgumentException(message.ToString());
+                    if (message.IsSuccessStatusCode)
+                    {
+                        var readTask = message.Content.ReadAsAsync<Inventory>();
+                        readTask.Wait();
+                        inventory = readTask.Result;
+                    }
                 }
+            }
+
+            catch (Exception ex)
+            {
+
             }
             return View(inventory);
 
--- ShopBridge/Models/Inventory.cs
+++ ShopBridge/Models/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,14 @@ namespace ShopBridge.Models
     {
 
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         public Nullable<decimal> Price { get; set; }
-        public bool Image { get; set; }
+        public string Image
+        {
+            get;set;
+        }
     }
 }
